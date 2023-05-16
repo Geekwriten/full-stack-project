bb219f3ea36c171e4e2441d0a04b802cbdd30a65
geekwriter <rougiernoa12@gmail.com>
2023-05-16 06:28:25 +0200

    almost there

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  database: 'mongodb+srv://rougiernoa12:rougiernoa12@testproject.oposa63.mongodb.net/?retryWrites=true&w=majority',
+  database: 'mongodb+srv://rougiernoa12:rougiernoa12@testproject.oposa63.mongodb.net',
     secret: 'yoursecret'
   }
